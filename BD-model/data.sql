8d6210063611fbbc4da638c123f57c847cad5b0c
KevinLliguisupa <kevinlliguisupa6@gmail.com>
2022-05-28 16:40:33 -0500

    Database model creation and script with data

---
+++ BD-model/data.sql
@@ -0,0 +1,123 @@
+--Users
+
+insert into users (usr_nickname, usr_name, usr_email, usr_password, usr_avatar, usr_state) values ('sgilbard0', 'Sibyl Gilbard', 'sgilbard0@godaddy.com', 'Wx5YymHA', 'https://robohash.org/reprehenderitautquia.png?size=50x50&set=set1', true);
+insert into users (usr_nickname, usr_name, usr_email, usr_password, usr_avatar, usr_state) values ('thorsell1', 'Tymon Horsell', 'thorsell1@marketwatch.com', 'SwHMD4Xev', 'https://robohash.org/quidemeteveniet.png?size=50x50&set=set1', true);
+insert into users (usr_nickname, usr_name, usr_email, usr_password, usr_avatar, usr_state) values ('nkeyhoe2', 'Normy Keyhoe', 'nkeyhoe2@360.cn', 'r3NN3CznREd', 'https://robohash.org/quiasequitemporibus.png?size=50x50&set=set1', true);
+insert into users (usr_nickname, usr_name, usr_email, usr_password, usr_avatar, usr_state) values ('mbreitler3', 'Merell Breitler', 'mbreitler3@exblog.jp', 'nHGPuQ', 'https://robohash.org/inventoredoloresdolor.png?size=50x50&set=set1', true);
+insert into users (usr_nickname, usr_name, usr_email, usr_password, usr_avatar, usr_state) values ('bdalyell4', 'Bil Dalyell', 'bdalyell4@pcworld.com', 'o8TpgUM6t', 'https://robohash.org/sitconsequaturvoluptatem.png?size=50x50&set=set1', true);
+insert into users (usr_nickname, usr_name, usr_email, usr_password, usr_avatar, usr_state) values ('mhartburn5', 'Marcela Hartburn', 'mhartburn5@archive.org', 'VsdpiuB', 'https://robohash.org/quismodinecessitatibus.png?size=50x50&set=set1', true);
+insert into users (usr_nickname, usr_name, usr_email, usr_password, usr_avatar, usr_state) values ('spaynes6', 'Shurwood Paynes', 'spaynes6@va.gov', '5FOSbzzzBtk2', 'https://robohash.org/quibusdamautdolore.png?size=50x50&set=set1', true);
+insert into users (usr_nickname, usr_name, usr_email, usr_password, usr_avatar, usr_state) values ('jwisbey7', 'Jody Wisbey', 'jwisbey7@virginia.edu', 'Zezl2XTGHrgD', 'https://robohash.org/utducimusest.png?size=50x50&set=set1', true);
+insert into users (usr_nickname, usr_name, usr_email, usr_password, usr_avatar, usr_state) values ('ncoomber8', 'Nert Coomber', 'ncoomber8@mayoclinic.com', 'RgdFN9B9Fu', 'https://robohash.org/errorquodolores.png?size=50x50&set=set1', true);
+insert into users (usr_nickname, usr_name, usr_email, usr_password, usr_avatar, usr_state) values ('nrowler9', 'Nikita Rowler', 'nrowler9@amazonaws.com', 'kB5tEb', 'https://robohash.org/temporeliberoipsam.png?size=50x50&set=set1', true);
+insert into users (usr_nickname, usr_name, usr_email, usr_password, usr_avatar, usr_state) values ('lbecksona', 'Lonna Beckson', 'lbecksona@disqus.com', 'LlXDg4mt', 'https://robohash.org/animisintaccusamus.png?size=50x50&set=set1', true);
+insert into users (usr_nickname, usr_name, usr_email, usr_password, usr_avatar, usr_state) values ('aellesmereb', 'Alf Ellesmere', 'aellesmereb@arizona.edu', 'v72u93', 'https://robohash.org/liberofugitearum.png?size=50x50&set=set1', true);
+insert into users (usr_nickname, usr_name, usr_email, usr_password, usr_avatar, usr_state) values ('sjensenc', 'Staci Jensen', 'sjensenc@statcounter.com', 'nZqntTKpd', 'https://robohash.org/corruptiremsed.png?size=50x50&set=set1', true);
+insert into users (usr_nickname, usr_name, usr_email, usr_password, usr_avatar, usr_state) values ('slongwoodd', 'Sherry Longwood', 'slongwoodd@vistaprint.com', '8TDMWOuPM', 'https://robohash.org/suscipitcorruptiaspernatur.png?size=50x50&set=set1', true);
+insert into users (usr_nickname, usr_name, usr_email, usr_password, usr_avatar, usr_state) values ('tmarshmane', 'Tiffani Marshman', 'tmarshmane@ed.gov', 'UdMdIJB', 'https://robohash.org/enimsintducimus.png?size=50x50&set=set1', true);
+insert into users (usr_nickname, usr_name, usr_email, usr_password, usr_avatar, usr_state) values ('marbucklef', 'Marni Arbuckle', 'marbucklef@businessweek.com', 'nuve6Xy1BfY', 'https://robohash.org/atetvoluptas.png?size=50x50&set=set1', true);
+insert into users (usr_nickname, usr_name, usr_email, usr_password, usr_avatar, usr_state) values ('lspurrierg', 'Lorne Spurrier', 'lspurrierg@imgur.com', 'vLrmDLvLNFYS', 'https://robohash.org/repellatexercitationemet.png?size=50x50&set=set1', true);
+insert into users (usr_nickname, usr_name, usr_email, usr_password, usr_avatar, usr_state) values ('adeaneh', 'Andonis Deane', 'adeaneh@last.fm', '6xbwWXX0J', 'https://robohash.org/nisirerumvoluptate.png?size=50x50&set=set1', true);
+insert into users (usr_nickname, usr_name, usr_email, usr_password, usr_avatar, usr_state) values ('agouldiei', 'Almire Gouldie', 'agouldiei@marketwatch.com', '6cL2owzEY7', 'https://robohash.org/etteneturut.png?size=50x50&set=set1', true);
+insert into users (usr_nickname, usr_name, usr_email, usr_password, usr_avatar, usr_state) values ('lgrinvalj', 'Laverna Grinval', 'lgrinvalj@sciencedaily.com', 'ntslW3d', 'https://robohash.org/voluptatumsaepeassumenda.png?size=50x50&set=set1', true);
+insert into users (usr_nickname, usr_name, usr_email, usr_password, usr_avatar, usr_state) values ('onasik', 'Orton Nasi', 'onasik@yolasite.com', 'ZzIrHV', 'https://robohash.org/nemononquisquam.png?size=50x50&set=set1', true);
+insert into users (usr_nickname, usr_name, usr_email, usr_password, usr_avatar, usr_state) values ('dpourvoieurl', 'Dalia Pourvoieur', 'dpourvoieurl@lycos.com', 'QJDKgP', 'https://robohash.org/teneturquinulla.png?size=50x50&set=set1', true);
+insert into users (usr_nickname, usr_name, usr_email, usr_password, usr_avatar, usr_state) values ('aashbem', 'Anstice Ashbe', 'aashbem@drupal.org', 'RFTYzFg5byyI', 'https://robohash.org/quiaeumea.png?size=50x50&set=set1', true);
+insert into users (usr_nickname, usr_name, usr_email, usr_password, usr_avatar, usr_state) values ('nclimon', 'Norene Climo', 'nclimon@bbb.org', 'WlQ3CN5UtXYD', 'https://robohash.org/praesentiumquaeculpa.png?size=50x50&set=set1', true);
+insert into users (usr_nickname, usr_name, usr_email, usr_password, usr_avatar, usr_state) values ('tcornewello', 'Trudey Cornewell', 'tcornewello@hc360.com', 'KCypLv5R', 'https://robohash.org/utetillum.png?size=50x50&set=set1', true);
+insert into users (usr_nickname, usr_name, usr_email, usr_password, usr_avatar, usr_state) values ('sjewkesp', 'Stesha Jewkes', 'sjewkesp@multiply.com', 'qc62sck', 'https://robohash.org/suntameta.png?size=50x50&set=set1', true);
+insert into users (usr_nickname, usr_name, usr_email, usr_password, usr_avatar, usr_state) values ('jschoroderq', 'Joaquin Schoroder', 'jschoroderq@dyndns.org', 'Kqr5zs09o', 'https://robohash.org/nonaliasquos.png?size=50x50&set=set1', true);
+insert into users (usr_nickname, usr_name, usr_email, usr_password, usr_avatar, usr_state) values ('jkabischr', 'Jewelle Kabisch', 'jkabischr@si.edu', 'wJ7zqmm1U3', 'https://robohash.org/quiaetreiciendis.png?size=50x50&set=set1', true);
+insert into users (usr_nickname, usr_name, usr_email, usr_password, usr_avatar, usr_state) values ('kshorys', 'Kimmy Shory', 'kshorys@webmd.com', 'TtrUFR6K0wJq', 'https://robohash.org/sitvitaerecusandae.png?size=50x50&set=set1', true);
+insert into users (usr_nickname, usr_name, usr_email, usr_password, usr_avatar, usr_state) values ('lpyettt', 'Lynn Pyett', 'lpyettt@apache.org', '9mt9H3', 'https://robohash.org/eafugitaliquid.png?size=50x50&set=set1', true);
+
+--Recipes
+
+insert into recipe (usr_id, rec_name, rec_description, rec_duration, rec_image, rec_state) values (24, 'Curabitur in libero ut massa volutpat convallis.', 'Maecenas ut massa quis augue luctus tincidunt. Nulla mollis molestie lorem. Quisque ut erat.', 1.5, 'http://dummyimage.com/200x200.png/dddddd/000000', true);
+insert into recipe (usr_id, rec_name, rec_description, rec_duration, rec_image, rec_state) values (16, 'Nullam porttitor lacus at turpis.', 'Phasellus sit amet erat. Nulla tempus. Vivamus in felis eu sapien cursus vestibulum.', 2.7, 'http://dummyimage.com/200x200.png/dddddd/000000', true);
+insert into recipe (usr_id, rec_name, rec_description, rec_duration, rec_image, rec_state) values (5, 'Maecenas tincidunt lacus at velit.', 'Praesent id massa id nisl venenatis lacinia. Aenean sit amet justo. Morbi ut odio.', 7.9, 'http://dummyimage.com/200x200.png/ff4444/ffffff', true);
+insert into recipe (usr_id, rec_name, rec_description, rec_duration, rec_image, rec_state) values (25, 'Suspendisse potenti.', 'Nullam sit amet turpis elementum ligula vehicula consequat. Morbi a ipsum. Integer a nibh.
+
+In quis justo. Maecenas rhoncus aliquam lacus. Morbi quis tortor id nulla ultrices aliquet.', 10.0, 'http://dummyimage.com/200x200.png/dddddd/000000', true);
+insert into recipe (usr_id, rec_name, rec_description, rec_duration, rec_image, rec_state) values (18, 'Donec ut dolor.', 'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus vestibulum sagittis sapien. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.', 5.5, 'http://dummyimage.com/200x200.png/ff4444/ffffff', true);
+insert into recipe (usr_id, rec_name, rec_description, rec_duration, rec_image, rec_state) values (26, 'Nulla tellus.', 'Praesent id massa id nisl venenatis lacinia. Aenean sit amet justo. Morbi ut odio.
+
+Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.', 9.3, 'http://dummyimage.com/200x200.png/cc0000/ffffff', true);
+insert into recipe (usr_id, rec_name, rec_description, rec_duration, rec_image, rec_state) values (7, 'In hac habitasse platea dictumst.', 'Fusce posuere felis sed lacus. Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl. Nunc rhoncus dui vel sem.', 10.3, 'http://dummyimage.com/200x200.png/5fa2dd/ffffff', true);
+insert into recipe (usr_id, rec_name, rec_description, rec_duration, rec_image, rec_state) values (25, 'Suspendisse potenti.', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla. Sed vel enim sit amet nunc viverra dapibus. Nulla suscipit ligula in lacus.
+
+Curabitur at ipsum ac tellus semper interdum. Mauris ullamcorper purus sit amet nulla. Quisque arcu libero, rutrum ac, lobortis vel, dapibus at, diam.', 3.6, 'http://dummyimage.com/200x200.png/cc0000/ffffff', true);
+insert into recipe (usr_id, rec_name, rec_description, rec_duration, rec_image, rec_state) values (7, 'Donec semper sapien a libero.', 'Aenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh.', 12.2, 'http://dummyimage.com/200x200.png/ff4444/ffffff', true);
+insert into recipe (usr_id, rec_name, rec_description, rec_duration, rec_image, rec_state) values (21, 'Duis at velit eu est congue elementum.', 'Aenean lectus. Pellentesque eget nunc. Donec quis orci eget orci vehicula condimentum.
+
+Curabitur in libero ut massa volutpat convallis. Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.', 4.1, 'http://dummyimage.com/200x200.png/ff4444/ffffff', true);
+insert into recipe (usr_id, rec_name, rec_description, rec_duration, rec_image, rec_state) values (28, 'Fusce lacus purus, aliquet.', 'Sed ante. Vivamus tortor. Duis mattis egestas metus.
+
+Aenean fermentum. Donec ut mauris eget massa tempor convallis. Nulla neque libero, convallis eget, eleifend luctus, ultricies eu, nibh.', 8.8, 'http://dummyimage.com/200x200.png/dddddd/000000', true);
+insert into recipe (usr_id, rec_name, rec_description, rec_duration, rec_image, rec_state) values (16, 'Donec quis orci eget orci vehicula condimentum.', 'Maecenas ut massa quis augue luctus tincidunt. Nulla mollis molestie lorem. Quisque ut erat.
+
+Curabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem.', 10.5, 'http://dummyimage.com/200x200.png/cc0000/ffffff', true);
+insert into recipe (usr_id, rec_name, rec_description, rec_duration, rec_image, rec_state) values (23, 'Proin leo odio, porttitor id.', 'Quisque porta volutpat erat. Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus.
+
+Phasellus in felis. Donec semper sapien a libero. Nam dui.', 13.9, 'http://dummyimage.com/200x200.png/cc0000/ffffff', true);
+insert into recipe (usr_id, rec_name, rec_description, rec_duration, rec_image, rec_state) values (13, 'In quis justo.', 'Curabitur in libero ut massa volutpat convallis. Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.
+
+Phasellus sit amet erat. Nulla tempus. Vivamus in felis eu sapien cursus vestibulum.', 2.6, 'http://dummyimage.com/200x200.png/ff4444/ffffff', true);
+insert into recipe (usr_id, rec_name, rec_description, rec_duration, rec_image, rec_state) values (6, 'Aenean lectus.', 'Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat.', 13.1, 'http://dummyimage.com/200x200.png/ff4444/ffffff', true);
+insert into recipe (usr_id, rec_name, rec_description, rec_duration, rec_image, rec_state) values (7, 'Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem.', 'In sagittis dui vel nisl. Duis ac nibh. Fusce lacus purus, aliquet at, feugiat non, pretium quis, lectus.
+
+Suspendisse potenti. In eleifend quam a odio. In hac habitasse platea dictumst.', 13.1, 'http://dummyimage.com/200x200.png/dddddd/000000', true);
+insert into recipe (usr_id, rec_name, rec_description, rec_duration, rec_image, rec_state) values (25, 'Integer a nibh.', 'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus vestibulum sagittis sapien. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+
+Etiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem.', 14.3, 'http://dummyimage.com/200x200.png/dddddd/000000', true);
+insert into recipe (usr_id, rec_name, rec_description, rec_duration, rec_image, rec_state) values (16, 'Praesent lectus.', 'Fusce consequat. Nulla nisl. Nunc nisl.
+
+Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa. Donec dapibus. Duis at velit eu est congue elementum.', 8.6, 'http://dummyimage.com/200x200.png/5fa2dd/ffffff', true);
+insert into recipe (usr_id, rec_name, rec_description, rec_duration, rec_image, rec_state) values (15, 'Nulla justo.', 'Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat.', 3.8, 'http://dummyimage.com/200x200.png/ff4444/ffffff', true);
+insert into recipe (usr_id, rec_name, rec_description, rec_duration, rec_image, rec_state) values (29, 'Proin interdum mauris non ligula.', 'Mauris enim leo, rhoncus sed, vestibulum sit amet, cursus id, turpis. Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci. Mauris lacinia sapien quis libero.', 8.0, 'http://dummyimage.com/200x200.png/cc0000/ffffff', true);
+
+--Ingredients
+
+insert into ingredient (ing_name, ing_image, ing_state) values ('Wine - Red, Mosaic Zweigelt', 'http://dummyimage.com/200x200.png/5fa2dd/ffffff', true);
+insert into ingredient (ing_name, ing_image, ing_state) values ('Alize Sunset', 'http://dummyimage.com/200x200.png/ff4444/ffffff', true);
+insert into ingredient (ing_name, ing_image, ing_state) values ('Pastrami', 'http://dummyimage.com/200x200.png/ff4444/ffffff', true);
+insert into ingredient (ing_name, ing_image, ing_state) values ('Ecolab Crystal Fusion', 'http://dummyimage.com/200x200.png/5fa2dd/ffffff', true);
+insert into ingredient (ing_name, ing_image, ing_state) values ('Appetizer - Mango Chevre', 'http://dummyimage.com/200x200.png/ff4444/ffffff', true);
+insert into ingredient (ing_name, ing_image, ing_state) values ('Napkin White', 'http://dummyimage.com/200x200.png/dddddd/000000', true);
+insert into ingredient (ing_name, ing_image, ing_state) values ('Venison - Striploin', 'http://dummyimage.com/200x200.png/5fa2dd/ffffff', true);
+insert into ingredient (ing_name, ing_image, ing_state) values ('Beef - Striploin', 'http://dummyimage.com/200x200.png/5fa2dd/ffffff', true);
+insert into ingredient (ing_name, ing_image, ing_state) values ('Radish - Pickled', 'http://dummyimage.com/200x200.png/ff4444/ffffff', true);
+insert into ingredient (ing_name, ing_image, ing_state) values ('Oil - Coconut', 'http://dummyimage.com/200x200.png/5fa2dd/ffffff', true);
+insert into ingredient (ing_name, ing_image, ing_state) values ('Cheese - Brie,danish', 'http://dummyimage.com/200x200.png/cc0000/ffffff', true);
+insert into ingredient (ing_name, ing_image, ing_state) values ('Arizona - Green Tea', 'http://dummyimage.com/200x200.png/ff4444/ffffff', true);
+insert into ingredient (ing_name, ing_image, ing_state) values ('Veal - Slab Bacon', 'http://dummyimage.com/200x200.png/cc0000/ffffff', true);
+insert into ingredient (ing_name, ing_image, ing_state) values ('Bamboo Shoots - Sliced', 'http://dummyimage.com/200x200.png/ff4444/ffffff', true);
+insert into ingredient (ing_name, ing_image, ing_state) values ('Noodles - Cellophane, Thin', 'http://dummyimage.com/200x200.png/dddddd/000000', true);
+insert into ingredient (ing_name, ing_image, ing_state) values ('Pickerel - Fillets', 'http://dummyimage.com/200x200.png/5fa2dd/ffffff', true);
+insert into ingredient (ing_name, ing_image, ing_state) values ('Cake - Pancake', 'http://dummyimage.com/200x200.png/dddddd/000000', true);
+insert into ingredient (ing_name, ing_image, ing_state) values ('Cheese - Perron Cheddar', 'http://dummyimage.com/200x200.png/cc0000/ffffff', true);
+insert into ingredient (ing_name, ing_image, ing_state) values ('Wine - Red, Metus Rose', 'http://dummyimage.com/200x200.png/dddddd/000000', true);
+insert into ingredient (ing_name, ing_image, ing_state) values ('Chips Potato Salt Vinegar 43g', 'http://dummyimage.com/200x200.png/ff4444/ffffff', true);
+
+--Ingredients-Recets
+
+insert into ing_rec (rec_id, ing_id, ing_rec_quantity, ing_rec_state) values (18, 6, 5, true);
+insert into ing_rec (rec_id, ing_id, ing_rec_quantity, ing_rec_state) values (17, 13, 2, true);
+insert into ing_rec (rec_id, ing_id, ing_rec_quantity, ing_rec_state) values (19, 16, 1, true);
+insert into ing_rec (rec_id, ing_id, ing_rec_quantity, ing_rec_state) values (5, 12, 5, true);
+insert into ing_rec (rec_id, ing_id, ing_rec_quantity, ing_rec_state) values (1, 3, 4, true);
+insert into ing_rec (rec_id, ing_id, ing_rec_quantity, ing_rec_state) values (16, 3, 4, true);
+insert into ing_rec (rec_id, ing_id, ing_rec_quantity, ing_rec_state) values (6, 3, 4, true);
+insert into ing_rec (rec_id, ing_id, ing_rec_quantity, ing_rec_state) values (7, 12, 5, true);
+insert into ing_rec (rec_id, ing_id, ing_rec_quantity, ing_rec_state) values (10, 8, 1, true);
+insert into ing_rec (rec_id, ing_id, ing_rec_quantity, ing_rec_state) values (18, 17, 2, true);
+insert into ing_rec (rec_id, ing_id, ing_rec_quantity, ing_rec_state) values (8, 6, 5, true);
+insert into ing_rec (rec_id, ing_id, ing_rec_quantity, ing_rec_state) values (2, 20, 4, true);
+insert into ing_rec (rec_id, ing_id, ing_rec_quantity, ing_rec_state) values (17, 15, 2, true);
+insert into ing_rec (rec_id, ing_id, ing_rec_quantity, ing_rec_state) values (18, 3, 4, true);
+insert into ing_rec (rec_id, ing_id, ing_rec_quantity, ing_rec_state) values (12, 6, 1, true);
+insert into ing_rec (rec_id, ing_id, ing_rec_quantity, ing_rec_state) values (2, 18, 3, true);
+insert into ing_rec (rec_id, ing_id, ing_rec_quantity, ing_rec_state) values (17, 9, 5, true);
+insert into ing_rec (rec_id, ing_id, ing_rec_quantity, ing_rec_state) values (15, 2, 4, true);
+insert into ing_rec (rec_id, ing_id, ing_rec_quantity, ing_rec_state) values (12, 12, 1, true);
+insert into ing_rec (rec_id, ing_id, ing_rec_quantity, ing_rec_state) values (10, 10, 2, true);
---
+++ BD-model/users-recipes.sql
@@ -0,0 +1,80 @@
+
+CREATE SEQUENCE public.ingredient_ing_id_seq;
+
+CREATE TABLE public.ingredient (
+                ing_id INTEGER NOT NULL DEFAULT nextval('public.ingredient_ing_id_seq'),
+                ing_name VARCHAR NOT NULL,
+                ing_image VARCHAR NOT NULL,
+                ing_state BOOLEAN NOT NULL,
+                CONSTRAINT ingredient_pk PRIMARY KEY (ing_id)
+);
+
+
+ALTER SEQUENCE public.ingredient_ing_id_seq OWNED BY public.ingredient.ing_id;
+
+CREATE SEQUENCE public.users_usr_id_seq;
+
+CREATE TABLE public.users (
+                usr_id INTEGER NOT NULL DEFAULT nextval('public.users_usr_id_seq'),
+                usr_nickname VARCHAR NOT NULL,
+                usr_email VARCHAR NOT NULL,
+                usr_password VARCHAR NOT NULL,
+                usr_avatar VARCHAR NOT NULL,
+                usr_name VARCHAR NOT NULL,
+                usr_state BOOLEAN NOT NULL,
+                CONSTRAINT users_pk PRIMARY KEY (usr_id)
+);
+
+
+ALTER SEQUENCE public.users_usr_id_seq OWNED BY public.users.usr_id;
+
+CREATE SEQUENCE public.recipe_rec_id_seq;
+
+CREATE TABLE public.recipe (
+                rec_id INTEGER NOT NULL DEFAULT nextval('public.recipe_rec_id_seq'),
+                usr_id INTEGER NOT NULL,
+                rec_name VARCHAR NOT NULL,
+                rec_description VARCHAR NOT NULL,
+                rec_duration NUMERIC(5,2) NOT NULL,
+                rec_image VARCHAR NOT NULL,
+                rec_state BOOLEAN NOT NULL,
+                CONSTRAINT recipe_pk PRIMARY KEY (rec_id)
+);
+
+
+ALTER SEQUENCE public.recipe_rec_id_seq OWNED BY public.recipe.rec_id;
+
+CREATE SEQUENCE public.ing_rec_ing_rec_id_seq;
+
+CREATE TABLE public.ing_rec (
+                ing_rec_id INTEGER NOT NULL DEFAULT nextval('public.ing_rec_ing_rec_id_seq'),
+                rec_id INTEGER NOT NULL,
+                ing_id INTEGER NOT NULL,
+                ing_rec_quantity NUMERIC(5,2) NOT NULL,
+                ing_rec_state BOOLEAN NOT NULL,
+                CONSTRAINT ing_rec_pk PRIMARY KEY (ing_rec_id)
+);
+
+
+ALTER SEQUENCE public.ing_rec_ing_rec_id_seq OWNED BY public.ing_rec.ing_rec_id;
+
+ALTER TABLE public.ing_rec ADD CONSTRAINT ingredient_ing_rec_fk
+FOREIGN KEY (ing_id)
+REFERENCES public.ingredient (ing_id)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION
+NOT DEFERRABLE;
+
+ALTER TABLE public.recipe ADD CONSTRAINT user_recipe_fk
+FOREIGN KEY (usr_id)
+REFERENCES public.users (usr_id)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION
+NOT DEFERRABLE;
+
+ALTER TABLE public.ing_rec ADD CONSTRAINT recipe_ing_rec_fk
+FOREIGN KEY (rec_id)
+REFERENCES public.recipe (rec_id)
+ON DELETE NO ACTION
+ON UPDATE NO ACTION
+NOT DEFERRABLE;
