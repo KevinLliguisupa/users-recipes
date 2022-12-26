cbb7810409de1df8a1783a460d738b4726b4fe70
KevinLliguisupa <kevinlliguisupa6@gmail.com>
2022-12-26 14:58:10 -0500

    add welcome message to api

---
+++ src/main/java/com/restapi/recipe/controller/GeneralController.java
@@ -0,0 +1,18 @@
+package com.restapi.recipe.controller;
+
+import com.restapi.recipe.dto.UsersInfoDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GeneralController {
+
+    @GetMapping
+    public String gretting(){
+        return "Welcome to Api recipes service";
+    }
+}
